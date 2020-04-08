f36c81d1726c5824251dde4f49a3a905b5f6becf
codecommituser <codecommituser@example.com>
2020-04-08 19:49:23 +1200

    changed region and used variables

--- m3/module_three.tf
+++ m3/module_three.tf
@@ -5,9 +5,9 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
-variable "key_name" {}
+variable "key_name" {} # EC2 key pair name
 variable "region" {
-  default = "us-east-1"
+  default = "ap-southeast-2"
 }
 
 ##################################################################################
@@ -15,7 +15,7 @@ variable "region" {
 ##################################################################################
 
 provider "aws" {
-  access_key = var.aws_access_key
+  access_key = var.aws_access_key # reading from TFVARS file
   secret_key = var.aws_secret_key
   region     = var.region
 }
@@ -83,13 +83,13 @@ resource "aws_instance" "nginx" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id] # list
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
-    private_key = file(var.private_key_path)
+    private_key = file(var.private_key_path) # function file
 
   }
 
